d2e6b27c5a1ad78c3774f3db15d45fb526311aec
Kaylie Sheehan <kaylie.sheehan@yahoo.com>
2021-05-01 13:58:13 -0500

    Update queries.sql

--- EmployeeSQL/queries.sql
+++ EmployeeSQL/queries.sql
@@ -7,4 +7,26 @@ on (e.emp_no = s.emp_no);
 --List first name, last name, and hire date for employees who were hired in 1986.
 select first_name, last_name, hire_date
 from employees
-where date_part('year', hire_date) = 1986
+where date_part('year', hire_date) = 1986;
+
+--List the manager of each department with the following information: department number, department name, the manager's employee number, last name, first name.
+select dp.dept_no, dp.dept_name, dm.emp_no, e.last_name, e.first_name
+from dept_manager dm
+join departments dp
+on (dm.dept_no = dp.dept_no)
+join employees e
+on (dm.emp_no = e.emp_no)
+
+--List the department of each employee with the following information: employee number, last name, first name, and department name.
+
+--List first name, last name, and sex for employees whose first name is "Hercules" and last names begin with "B."
+select e.first_name, e.last_name, e.sex
+from employees e
+where (e.first_name = 'Hercules')
+and (e.last_name like 'B%')
+
+--List all employees in the Sales department, including their employee number, last name, first name, and department name.
+
+--List all employees in the Sales and Development departments, including their employee number, last name, first name, and department name.
+
+--In descending order, list the frequency count of employee last names, i.e., how many employees share each last name.
